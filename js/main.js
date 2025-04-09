50578a306dd6e90c888cf5bd6f97525e229c27c3
andrea-mosca <andrea.moscateli@gmail.com>
2025-04-09 15:23:54 +0200

    bonus

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 const lampButton = document.getElementById("lamp-button");
 const lampImg = document.getElementById("lampadina-img");
 
+// lampButton.addEventListener("click", function() {
+//     lampImg.src = "./img/yellow_lamp.png";
+//     lampImg.style.transform = "scaleX(-1)"; // specchia la nuova immagine per sostituirla nella stessa posizione visiva della precedente
+//   });
+
+
 lampButton.addEventListener("click", function() {
-    lampImg.src = "./img/yellow_lamp.png";
-    lampImg.style.transform = "scaleX(-1)"; // specchia la nuova immagine per sostituirla nella stessa posizione visiva della precedente
+    if (lampButton.textContent.includes("Accendi")) {
+      lampImg.src = "./img/yellow_lamp.png";
+      lampImg.style.transform = "scaleX(-1)";
+      lampButton.textContent = "Spegni";
+    } else {
+      lampImg.src = "./img/white_lamp.png";
+      lampImg.style.transform = "scaleX(1)";
+      lampButton.textContent = "Accendi";
+    }
   });
